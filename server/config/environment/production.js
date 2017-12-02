754f9e7369b2829d0ed59fe4ba29f3d7a0e1d0c7
ritz <ritesh.salesforce@gmail.com>
2017-12-02 23:39:20 +0530

    Set key properly

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -32,7 +32,7 @@ module.exports = {
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
     project_id: process.env.FIREBASE_PROJECT_ID,
     private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    private_key: JSON.parse(process.env.FIREBASE_PRIVATE_KEY),
     client_id: process.env.FIREBASE_CLIENT_ID,
     auth_uri: process.env.FIREBASE_AUTH_URI,
     token_uri: process.env.FIREBASE_TOKEN_URI,
